638f630b5af1de725cd8313b1988583e10fedee1
David Koenig <34135598+Frickeldave@users.noreply.github.com>
2025-08-08 13:49:10 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/script.js",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
